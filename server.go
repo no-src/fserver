31340a46e8ea245e7f45eb2d92ca2cc76e74baf0
mstmdev <mstmdev@gmail.com>
2023-10-02 00:05:13 +0800

    Add some comments (#7)

--- server.go
+++ server.go
@@ -9,6 +9,7 @@ import (
 	"github.com/no-src/log"
 )
 
+// Run start the static file web server
 func Run(port int, pathPrefix string, dist embed.FS) {
 	addr := fmt.Sprintf(":%d", port)
 	url := "http://127.0.0.1" + addr
@@ -30,6 +31,7 @@ type fsPrefixHandler struct {
 	handler http.Handler
 }
 
+// ServeHTTP implement the http.Handler interface
 func (h *fsPrefixHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	r.URL.Path = h.prefix + r.URL.Path
 	h.handler.ServeHTTP(w, r)
